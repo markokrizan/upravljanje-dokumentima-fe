aaf077fa3c3ad119aaf36505e49a49c1dd2bb7d9
Marko Krizan <markokrizan64@gmail.com>
2020-07-01 17:59:36 +0200

    Show single message in modal

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,60 +1,54 @@
 import React from 'react'
+import { isEmpty } from 'lodash';
+
 import './Message.css';
 
-export default function Message() {
+export default function Message({ message }) {
+    if(!message || isEmpty(message)) {
+        return (<div>Error rendering message! </div>)
+    }
+
     return (
-    <div className="container">
-        <div className="row">
-            <div className="col-md-12 d-flex justify-content-center">
-            <div className="card text-center">
-                <div className="card-header"> Message<br/></div>
-                <div className="card-body">
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>From:</label></div>
-                    <div className="col-md-6 d-flex justify-content-start">
-                    <labe>pera@mail.com</labe>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>To:</label></div>
-                    <div className="col-md-6 d-flex justify-content-start">
-                    <labe>pera@mail.com</labe>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Cc:</label></div>
-                    <div className="col-md-6 d-flex justify-content-start">
-                    <labe>pera@mail.com</labe>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Bcc:</label></div>
-                    <div className="col-md-6 d-flex justify-content-start">
-                    <labe>pera@mail.com</labe>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Date:</label></div>
-                    <div className="col-md-6 d-flex justify-content-start">
-                    <labe>12/123/12</labe>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Subject:</label></div>
-                    <div className="col-md-6 d-flex justify-content-start">
-                    <labe>Neki subject</labe>
-                    </div>
-                </div>
-                <div className="row content-container">
-                    <div className="col-md-12 border"> content kasjdlfkj akjsdflk jasdklfj klasdjf klasjdfklasjdfkljasdklf jasdklfj aklsdfj klasdfjklasdjfklasjdf </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-12 d-flex justify-content-end mt-2"><a className="btn btn-success" href="#">Close</a></div>
-                </div>
+        <div>
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>From:</label></div>
+                <div className="col-md-6 d-flex justify-content-start">
+                    <label>{message.from && message.from}</label>
                 </div>
             </div>
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>To:</label></div>
+                <div className="col-md-6 d-flex justify-content-start">
+                    <label>{message.to && message.to}</label>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Cc:</label></div>
+                <div className="col-md-6 d-flex justify-content-start">
+                    <label>{message.cc && message.cc}</label>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Bcc:</label></div>
+                <div className="col-md-6 d-flex justify-content-start">
+                    <label>{message.bcc && message.bcc}</label>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Date:</label></div>
+                <div className="col-md-6 d-flex justify-content-start">
+                    <label>{message.dateTime && message.dateTime}</label>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 d-flex align-items-center border-right justify-content-end"><label>Subject:</label></div>
+                <div className="col-md-6 d-flex justify-content-start">
+                    <label>{message.subject && message.subject}</label>
+                </div>
+            </div>
+            <div className="row content-container">
+                <div className="col-md-12 border">{message.content && message.content || 'No content'}</div>
             </div>
         </div>
-    </div>
     )
 }
--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import Modal from '../Modal'
+import Message from '../Message';
 
 export default function MessageList({ messages }) {
+    const [modalShow, setModalShow] = useState(false);
+    const [currentMessage, setCurrentMessage] = useState(null);
+
     const renderTitle = message => {
         return message.from && message.subject && `${message.from} - ${message.subject}` || '(No subject)';
     }
 
+    const showMessage = message => {
+        setCurrentMessage(message);
+        setModalShow(true);
+    }
+
     const renderMessageItems = () => {
         return messages && messages.messages && messages.messages.length ? (
-            <ul className="list-group w-100 h-100">
-                {messages.messages.map(message => (
-                    <Link 
-                        to={`/message/${message.id}`}
-                        className="
-                            list-group-item 
-                            list-group-item-action 
-                            d-flex 
-                            justify-content-between 
-                            align-items-center"
-                    >
-                        {renderTitle(message)}
-                    </Link>
-                ))}
-            </ul>) : (
+            <>
+                <ul className="list-group w-100 h-100">
+                    {messages.messages.map(message => (
+                        <div 
+                            onClick={() => showMessage(message)}
+                            className="
+                                list-group-item 
+                                list-group-item-action 
+                                d-flex 
+                                justify-content-between 
+                                align-items-center"
+                        >
+                            {renderTitle(message)}
+                        </div>
+                    ))}
+                </ul>
+                <Modal
+                    show={modalShow}
+                    header={'Message'}
+                    body={
+                        <Message message={currentMessage} />}
+                    actions={<button className="btn btn-success" onClick={() => setModalShow(false)}>Close</button>}
+                />
+            </>) : (
                 <div>No messages!</div>
             );
     }
